6556af2ac3b2553c6d710768e20a9c184f72d76d
imaginevinay <vinay.varshney28@gmail.com>
2026-02-18 11:51:16 +0530

    integrated graftcode with next.js

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+class MyService {
+  constructor() {}
+
+  async hello() {
+    return "Hello from the Graftcode Backend!";
+  }
+}
+
+export default MyService;
---
+++ frontend/next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["@graft/npm-graft-backend"],
+};
+
+export default nextConfig;
